8cb5eef0399439400b8278f79f42f6e39394b51d
root <shamsdhar@gmail.com>
2020-01-21 23:41:21 -0500

    day1

---
+++ strings.py
@@ -0,0 +1,31 @@
+"""here I will also talk about comments see comments are the way to define a code or remaark a code lets see an example"""
+a=input("enter value of a")#a stores value of pizza
+b=input('enter the value of b')#b stores value of burger
+print("I love ",a)
+"""so here we can see we used commnets to remark a code but strings inside the comments will not be executed """
+"""
+# this symbol is used for one line comment
+
+"""
+'''this is used for multiple
+line comment'''
+"""this symbol is doc string python documentation strings provide a convinient way of associating documentation with python modules and classes and functions and methods. These triple qoutations are used for docstring and these docstrings can be accessed using __doc__ lets see an example:
+"""
+"""This is an example of docstring in python"""
+print("I am the shitty docstring you were looking for :",__doc__)
+"""if you want to use text in python we use strings to display string in python console then we use double or singleqoutations in console"""
+"""eg:
+>>> "Python is fun!"
+'Python is fun!'
+>>> 'Always look on the bright side of life'
+'Always look on the bright side of life'
+"""
+"""Some characters can't be directly included in a string. For instance, double quotes can't be directly included in a double quote string; this would cause it to end prematurely. here comes the hero Mr Backslash "\" 
+eg:
+"""
+"""
+Newlines:
+
+Python provides an easy way to avoid manually writing "\n" to escape newlines in a string. Create a string with three sets of quotes, and newlines that are created by pressing Enter are automatically escaped for you."""
+print("\n")
+print("Mr Shams\'s crush hates him but \nshams love his crush and also crush\'s teacher")
