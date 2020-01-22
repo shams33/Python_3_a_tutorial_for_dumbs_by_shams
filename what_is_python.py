92895a31d85a4ce38c04ab65095aa20ee11bbb05
root <shamsdhar@gmail.com>
2020-01-21 20:41:56 -0500

    day1

---
+++ what_is_python.py
@@ -0,0 +1,57 @@
+"""python is a high level programming language,with applications in numerous areas ,including web programming ,scripting,scientific computing, and artificial intelligence
+
+>python is a cross-platform language and easy to learn you can build your own APIs or web crawler or web scrapper or websites or you can make calls  or message using python building automated bots and building hacking tools using python and you can do machine learning statistical models or data science using python and natural language processing too but sorry to say just besides iphone or android apps but you can make there parts of it
+
+>>features of python:
+Easy to learn
+scalable :python supports better structure and better platform for large programs
+interpreted language and it supports user interactive mode
+GUI :python supports GUI applications that can be created and ported to many system calls and library and windows system
+
+
+"""
+print(("""Welcome to Python"""))
+
+"""there are 3 major version of python that are 1.x,2.x,3.x and there minor versions which are subdivided into minor versions,such as 2.7 and 3.3.
+code written for python 3.x is guaranteed to work in all future versions.
+
+there are somany flavours of python such as 
+#cpython : yesss your python is actually c python 
+#Jython which is actually succesor of jpython which is actually designed to run on java platform
+#Ironpython thats  for .NET Framework
+#PyPy:its actual focus is to overcome drawbacks of python
+#stackless python : if you ever tried threading then you can understand what is stackless python it is actually reimplentation of traditional python to implement lightweight threading .If you knnow about softirqs,tasklets or BottomHalves then its easy to understand .let's say about these brief its nothing just software interrupt context or we can say whenever a sytem call is about to return to userspace then any interrupts marked pending are run and much of the real interrupt handling is done here. During stone age of computer there were only Bottom Halves and now people uses softirqs but this is a new shit to deal with so tasklets are used btw it has no relation with tasks and probably more to do deal with some bad country spirit had at the time. 
+jokes apart with this you can control scheduling of tasklets
+PythonXY: it is nothing but scintific python distribution
+portable python is like having python anytime anywhere no matter if you are pooping or watching porn its for windowsOS and you just have to extract it to your portable storage device and bang you are ready to use"""
+
+
+"""python 2 vs 3
+>Division operator problem 
+##a=12
+b=8
+print(a%b)
+run this in python 3 and python 2 you will understand the problem
+> Xrange : in python 2 there were x range but here we dont have xrange in 3
+>__future__: 
+#from __future__ import division
+ print(12/8)   //run it in python 2.x
+ #Python virtual Machine (PVM): Python programming language is in this world from 1989 and it has since grown a lot and its first version was started by Guido van rossum in 1989
+ PVM consists of steps such as Intialisation in which set up of various data structure needed by python processor is done and then the stage comes 
+ Compilig in which building of syntax trees and symbol table building is done and last but not the least is 
+ Interpreting: its actually execution of code
+ 
+Frozen Bianaries in python: it is like burger king where the whole burger is a single package which is  made with your byte of code of your pragram files along with PVM and any python support files your program need just like lettuce,sauce and mayo
+ today like three primary systems are capable of this frozen bianaries which are like py2exe(windows),PyInstaller(similar to py2exe linux and unix) and freeze 
+ """
+"""Installation of python
+please google it and learn as if I will help you will not research and use your fucking brain
+"""
+"""IDE's Integrated development environment is the kitchen where you cook your  code it consist of source code editor and debugger:
+>PyCharm: it is developed by czech company Jet Brains and it supports web framework such as django,flask and web2py and also it supports integrated unit tasting and also it support VCS which is version control system . This software was founded on 2011 but the open source version became available on 2014.
+>Spyder:it is like scintific IDE for python which consist of special lscintific library and modules such as numpy,matplotlib,scipy,Ipython and also it supports special GUI module which is pyqt5
+>jupyter:lets see what it says in their website "The Jupyter Notebook is an open-source web application that allows you to create and share documents that contain live code, equations, visualizations and narrative text. Uses include: data cleaning and transformation, numerical simulation, statistical modeling, data visualization, machine learning, and much more."
+
+ """
+print("today is 2030 and I am fucking society with my own revolutionary movement !!!long live meritocracy ") #first  code of your python
+
