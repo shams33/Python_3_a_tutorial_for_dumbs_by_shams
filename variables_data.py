617d23c665bf1861b09a5ca3e91edae020915601
root <shamsdhar@gmail.com>
2020-01-25 16:21:18 -0500

    day2

--- variables_data.py
+++ variables_data.py
@@ -1,1 +1,2 @@
 """variables and data types"""
+"""there are total 33 keyword in python and keyword is a word that is reserved for a program and aa user cannot use it as user defined word in entire program  """
