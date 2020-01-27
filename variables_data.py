1587b1fe549507d196ef9ca0fcc4cd985d62f751
root <shamsdhar@gmail.com>
2020-01-27 01:35:22 -0500

    day2

--- variables_data.py
+++ variables_data.py
@@ -1,2 +1,10 @@
 """variables and data types"""
 """there are total 33 keyword in python and keyword is a word that is reserved for a program and aa user cannot use it as user defined word in entire program  """
+"""
+variables are like cup to store tea so we can say variables are containers for storing  data values
+python is not like c,c++  python  has no command for declaring a variable
+ in python a variable is created the moment you first assign a value to it"""
+x = "I will not miss my ex ,I will move on"
+y = ":( she also used to drink water"
+print(x)
+print(y)
